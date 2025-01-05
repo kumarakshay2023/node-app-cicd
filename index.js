0426c5d47c25a64d155c0f8ac5cfeed8426410c9
kumarakshay2023 <53837497+kumarakshay2023@users.noreply.github.com>
2025-01-05 23:58:20 +0530

    code updated

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
+app.get('/api/call', (req, res) => {
+    res.send('Hello, World')  
+})
 app.get('/api/products', (req, res) => { 
     res.send('Products listing')
 })
